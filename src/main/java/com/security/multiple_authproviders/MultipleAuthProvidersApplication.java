3f3d43a84253003574b6491653c056f7e08edf7f
rico <rico@localhost.localdomain>
2023-10-29 07:52:16 +0300

    iniliazed a new project

---
+++ src/main/java/com/security/multiple_authproviders/MultipleAuthProvidersApplication.java
@@ -0,0 +1,13 @@
+package com.security.multiple_authproviders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleAuthProvidersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleAuthProvidersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/multiple_authproviders/MultipleAuthProvidersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.multiple_authproviders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleAuthProvidersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
